216a7c42626cac9a8f0f8b41ba57385b5b692bd9
Clement-Z4RM <clement.lienard@epitech.eu>
2024-09-23 20:59:34 +0200

    feat(backend): add login route

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
+import { type LoginResponse } from '@common/types/API/login/LoginResponse';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
+
+    @Post('login')
+    login(@Req() request: Request): LoginResponse {
+        return this.appService.login(request);
+    }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Req } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { parseCredentials } from '@common/types/API/login/Credentials';
+import { type LoginResponse } from '@common/types/API/login/LoginResponse';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    constructor(private readonly configService: ConfigService) {}
+
+    getHello(): string {
+        return 'Hello World!';
+    }
+
+    // TODO: find why object fields in LoginResponse are not required
+    login(@Req() request: Request): LoginResponse {
+        const credentials = parseCredentials(request.body);
+
+        if (!credentials.success) {
+            return {
+                code: 400,
+                message: 'Invalid credentials',
+                errors: credentials.error.format(),
+            };
+        }
+        return {
+            code: 200,
+            message: 'Success',
+            data: {
+                token: this.configService.get('JWT_SECRET'),
+            },
+        };
+    }
 }
