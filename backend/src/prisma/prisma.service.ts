1832b9c2833b06847dd0e51d555de0a66e5c5db4
DURAND Malo <malo.durand@epitech.eu>
2024-09-24 20:55:08 +0200

    backend(fix): untracked service folders.

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(private readonly configService: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: configService.get('POSTGRESQL_URL'),
+        },
+      },
+    });
+  }
+
+  onModuleInit() {
+    return this.$connect();
+  }
+
+  onModuleDestroy() {
+    return this.$disconnect();
+  }
+}
---
+++ backend/src/users/interfaces/user.interface.ts
@@ -0,0 +1,8 @@
+export interface User {
+  readonly id: string;
+  readonly email: string;
+  readonly hashed_password: string;
+  readonly firstname: string;
+  readonly lastname: string;
+  readonly is_admin: boolean;
+}
