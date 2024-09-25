4737d74e5dda579b1fc98c973602ef6023d7c58d
Clement-Z4RM <clement.lienard@epitech.eu>
2024-09-25 22:43:00 +0200

    build(common): add ESLint and tsc
    Add and configure ESLint (with Prettier).
    Add ESLint and Prettier scripts.
    Install typescript to have tsc and add a script to execute it.
    Also apply ESLint/Prettier style on source files.

--- common/src/types/API/login/LoginResponse.ts
+++ common/src/types/API/login/LoginResponse.ts
@@ -6,14 +6,16 @@
  * @property {string} [data.token] - The JWT token (if code is 200).
  * @property {string[]} [errors] - The credentials parsing error (if code is 400).
  */
-export type LoginResponse = {
-    code: 200;
-    message: "Success";
-    data: {
-        token: string;
-    };
-} | {
-    code: 400;
-    message: "Invalid credentials";
-    errors: string[]
-};
+export type LoginResponse =
+    | {
+          code: 200;
+          message: "Success";
+          data: {
+              token: string;
+          };
+      }
+    | {
+          code: 400;
+          message: "Invalid credentials";
+          errors: string[];
+      };
