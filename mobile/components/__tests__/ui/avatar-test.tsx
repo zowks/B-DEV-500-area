ec33175a83dc0ebc83d1e4cd4e91f6f75727d60a
Clement-Z4RM <clement.lienard@epitech.eu>
2024-09-29 01:03:46 +0200

    test(mobile): use `screen.getByText` instead of `getByText` returned by `render` following `eslint-plugin-testing-library` recommandations

--- mobile/components/__tests__/ui/avatar-test.tsx
+++ mobile/components/__tests__/ui/avatar-test.tsx
@@ -1,19 +1,18 @@
-import { render } from "@testing-library/react-native";
+import { render, screen } from "@testing-library/react-native";
 
 import { Avatar, AvatarFallback } from "~/components/ui/avatar";
 import { Text } from "~/components/ui/text";
 
 describe("<Avatar />", () => {
     test("AvatarFallback renders when no AvatarImage is provided to Avatar", () => {
-        const { getByText } = render(
+        render(
             <Avatar alt="Hello, World! I'm Avatar alt." className='w-24 h-24'>
                 <AvatarFallback>
                     <Text>Hello, World! I'm AvatarFallback.</Text>
                 </AvatarFallback>
             </Avatar>
         );
-
-        getByText("Hello, World! I'm AvatarFallback.");
+        screen.getByText("Hello, World! I'm AvatarFallback.");
     });
 
     test("Avatar without AvatarImage renders correctly", () => {
