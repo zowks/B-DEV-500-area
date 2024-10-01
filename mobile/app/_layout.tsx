df47b499735cd80c396234f7d0c0c178d86978dc
Clement-Z4RM <clement.lienard@epitech.eu>
2024-10-01 12:45:19 +0200

    feat(mobile): replace i18n package with `i18next` and initialize it

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,29 +1,20 @@
 import "~/global.css";
 
+import "~/src/i18n/i18n";
+
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { type Theme, ThemeProvider } from "@react-navigation/native";
 import { SplashScreen, Stack } from "expo-router";
-import { getLocales } from "expo-localization";
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import useMount from "react-use/lib/useMount";
 import { Platform } from "react-native";
+import { useTranslation } from "react-i18next";
 import { NAV_THEME } from "~/lib/constants";
 import { useColorScheme } from "~/lib/useColorScheme";
 import { PortalHost } from "@rn-primitives/portal";
-import { LocaleToggle } from "~/components/LocaleToggle";
 import { ThemeToggle } from "~/components/ThemeToggle";
 import { setAndroidNavigationBar } from "~/lib/android-navigation-bar";
-import { useI18nContext } from "~/src/i18n/i18n-react";
-import { isLocale } from "~/src/i18n/i18n-util";
-import { loadLocaleAsync } from "~/src/i18n/i18n-util.async";
-import { getUserLocale } from "~/src/i18n/utils";
-import type { Locales } from "~/src/i18n/i18n-types";
-import TypesafeI18n from "~/src/i18n/i18n-react";
-
-const DEFAULT_LOCALE = getLocales()
-    .map(locale => locale.languageCode)
-    .find(locale => locale);
 
 const LIGHT_THEME: Theme = {
     dark: false,
@@ -42,11 +33,11 @@ export {
 // Prevent the splash screen from auto-hiding before getting the color scheme.
 SplashScreen.preventAutoHideAsync();
 
-function Layout() {
+export default function RootLayout() {
     const { colorScheme, setColorScheme, isDarkColorScheme } = useColorScheme();
     const [isColorSchemeLoaded, setIsColorSchemeLoaded] = useState(false);
 
-    const { LL } = useI18nContext();
+    const { t } = useTranslation();
 
     useMount(() => {
         (async () => {
@@ -86,37 +77,12 @@ function Layout() {
                 <Stack.Screen
                     name="index"
                     options={{
-                        title: LL.HI({ name: "John" }),
-                        headerRight: () => (
-                            <div className="flex">
-                                <LocaleToggle />
-                                <ThemeToggle />
-                            </div>
-                        )
+                        title: t("Welcome to React"),
+                        headerRight: () => <ThemeToggle />
                     }}
                 />
             </Stack>
             <PortalHost />
         </ThemeProvider>
-    );
-}
-
-export default function RootLayout() {
-    const [localeLoaded, setLocaleLoaded] = useState<Locales | null>(null);
-
-    useMount(() => {
-        getUserLocale(DEFAULT_LOCALE && isLocale(DEFAULT_LOCALE) ? DEFAULT_LOCALE : "en")
-            .then(async locale => {
-                await loadLocaleAsync(locale);
-                return locale;
-            }).then(setLocaleLoaded);
-    });
-
-    if (!localeLoaded) return null;
-
-    return (
-        <TypesafeI18n locale={localeLoaded}>
-            <Layout />
-        </TypesafeI18n>
     );
 }
