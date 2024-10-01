203cf9c01ca4e8088a433f668c9fd3a2993db36a
DURAND Malo <malo.durand@epitech.eu>
2024-10-01 14:30:53 +0200

    fix(backend): tests related to credentials.

--- backend/src/discord/discord.service.spec.ts
+++ backend/src/discord/discord.service.spec.ts
@@ -1,15 +1,18 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { HttpModule } from "@nestjs/axios";
 import { DiscordService } from "./discord.service";
-import { CredentialsModule } from "../credentials/credentials.module";
+import { DiscordCredentialsService } from "./discord_credentials.service";
 
 describe("DiscordService", () => {
     let service: DiscordService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            imports: [CredentialsModule, HttpModule],
-            providers: [DiscordService]
+            imports: [HttpModule],
+            providers: [
+                { provide: DiscordCredentialsService, useValue: {} },
+                DiscordService
+            ]
         }).compile();
 
         service = module.get<DiscordService>(DiscordService);
--- backend/src/youtube/youtube.service.spec.ts
+++ backend/src/youtube/youtube.service.spec.ts
@@ -1,15 +1,18 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { HttpModule } from "@nestjs/axios";
 import { YouTubeService } from "./youtube.service";
-import { CredentialsModule } from "src/credentials/credentials.module";
+import { YoutubeCredentialsService } from "./youtube_credentials.service";
 
 describe("YoutubeService", () => {
     let service: YouTubeService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            imports: [CredentialsModule, HttpModule],
-            providers: [YouTubeService]
+            imports: [HttpModule],
+            providers: [
+                { provide: YoutubeCredentialsService, useValue: {} },
+                YouTubeService
+            ]
         }).compile();
 
         service = module.get<YouTubeService>(YouTubeService);
