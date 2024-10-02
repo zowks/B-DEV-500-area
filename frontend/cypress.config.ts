632db2af893661f8f886f276fbf66d733e202fc9
Clement-Z4RM <clement.lienard@epitech.eu>
2024-10-02 16:16:05 +0200

    test(frontend): setup tests with Cypress

---
+++ frontend/cypress.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+    e2e: {
+        setupNodeEvents() {
+            // implement node event listeners here
+        }
+    },
+    component: {
+        devServer: {
+            framework: "svelte",
+            bundler: "vite"
+        }
+    }
+});
