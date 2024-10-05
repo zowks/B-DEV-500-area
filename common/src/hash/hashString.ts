37ab7adeab5079160bd3fe6401a2097a9eaaa611
Clement-Z4RM <clement.lienard@epitech.eu>
2024-10-05 13:35:06 +0200

    fix(common): use `crypto` from ` window`

--- common/src/hash/hashString.ts
+++ common/src/hash/hashString.ts
@@ -1,5 +1,5 @@
 export default async function hashString(string: string, algorithm: string = "SHA-512") {
-    const hashBuffer = await crypto.subtle.digest(algorithm, (new TextEncoder()).encode(string));
+    const hashBuffer = await window.crypto.subtle.digest(algorithm, (new TextEncoder()).encode(string));
     const hashArray = Array.from(new Uint8Array(hashBuffer));
 
     return hashArray.map(byte => byte.toString(16).padStart(2, "0")).join("");
