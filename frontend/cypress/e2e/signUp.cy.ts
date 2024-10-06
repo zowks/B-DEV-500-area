a630b77c170562d80579c7c21a76a1915330c7c8
Clement-Z4RM <clement.lienard@epitech.eu>
2024-10-06 16:33:13 +0200

    test(frontend-auth): add sign in to sign up test

--- frontend/cypress/e2e/signUp.cy.ts
+++ frontend/cypress/e2e/signUp.cy.ts
@@ -1,23 +1,26 @@
 describe("authentication", () => {
     it("sign-up", function() {
+        const email = `email-${Date.now()}@example.com`;
+
         cy.visit(Cypress.env("CYPRESS_HOST"));
-        cy.location("pathname", { timeout: 60000 }).should("include", "/auth/sign-in");
+        cy.location("pathname", { timeout: 60000 }).should("eq", "/en/auth/sign-in");
         cy.get("h1").contains("Sign in").should("be.visible");
         cy.get("a").contains("Sign up").click();
-        cy.location("pathname", { timeout: 60000 }).should("include", "/auth/sign-up");
+        cy.location("pathname", { timeout: 60000 }).should("eq", "/en/auth/sign-up");
         cy.get("h1").contains("Sign up").should("be.visible");
-        cy.get("input#email").type(`email-${Date.now()}@example.com`);
+        cy.get("input#email").type(email);
         cy.get("input#firstname").type("John");
         cy.get("input#lastname").type("Doe");
         cy.get("input#password").type("password");
         cy.contains("Accept terms and conditions").click();
         cy.get("button").contains("Sign up").click();
-        cy.location("pathname", { timeout: 60000 }).should("include", "/auth/sign-in");
+        cy.location("pathname", { timeout: 60000 }).should("eq", "/en/auth/sign-in");
         cy.get("h1").contains("Sign in").should("be.visible");
         cy.get("#email").clear();
-        cy.get("#email").type("email@example.com");
+        cy.get("#email").type(email);
         cy.get("#password").clear();
         cy.get("#password").type("password");
         cy.get("button").contains("Sign in").click();
+        cy.location("pathname", { timeout: 60000 }).should("eq", "/en");
     });
 });
