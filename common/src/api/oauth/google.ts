7b773664634846e88a1a2098fb81eb4e8bc21d1e
Clement-Z4RM <clement.lienard@epitech.eu>
2024-10-07 14:18:49 +0200

    feat(common-api): return set cookie from `/oauth/google`

--- common/src/api/oauth/google.ts
+++ common/src/api/oauth/google.ts
@@ -6,7 +6,7 @@ type OAuthGooglePayload = {
 };
 
 // TODO: common response type
-export default async function google(apiUrl: string, payload: OAuthGooglePayload, accessToken: string): Promise<RequestResponse<{ redirect_uri: string; }, 200 | 401>> {
+export default async function google(apiUrl: string, payload: OAuthGooglePayload, accessToken: string): Promise<RequestResponse<{ redirect_uri: string; connectCookie: string; }, 200 | 401>> {
     try {
         const response = await fetch(`${apiUrl}/oauth/google?redirect_uri=${payload.redirect_uri}&scope=${payload.scope}`, {
             method: "GET",
@@ -15,7 +15,7 @@ export default async function google(apiUrl: string, payload: OAuthGooglePayload
 
         switch (response.status) {
         case 200:
-            return { status: 200, success: true, body: await response.json() };
+            return { status: 200, success: true, body: { ...(await response.json()), connectCookie: response.headers.getSetCookie()[0] } }; // TODO: auto set-cookie?
         case 401:
             return { status: 401, success: false }; // This route is protected. The client must supply a Bearer token.
         default:
