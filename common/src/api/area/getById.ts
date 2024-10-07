a0189e806b00bb3328a417c390e9e035d1c2cc6b
Zowks-VI <cyffers.merlin@gmail.com>
2024-10-07 00:48:02 +0000

    style(common-api): Fixing bad indentation

--- common/src/api/area/getById.ts
+++ common/src/api/area/getById.ts
@@ -1,22 +1,22 @@
-import { Area } from "../../types/area/interfaces/area.interface";
-import { RequestResponse } from "../api";
+import type { RequestResponse } from "../api";
+import type { Area } from "../../types/area/interfaces/area.interface";
 
 export default async function getById(apiUrl: string, accessToken: string, areaId: string): Promise<RequestResponse<Area, 200 | 401>> {
     try {
         const response = await fetch(`${apiUrl}/area/${areaId}`, {
-            method: 'GET',
+            method: "GET",
             headers: { Authorization: `Bearer ${accessToken}` }
         });
 
         switch (response.status) {
-            case 201: // TODO: Fix this response status / description with @Ximaz
-                return { status: 200, success: true, body: await response.json() };
-            case 401:
-                return { status: 401, success: false }; // Either the JWT is expired or invalid or the user has been deleted.
-            default:
-                return { status: 500, success: false }
+        case 201: // TODO: Fix this response status / description with @Ximaz
+            return { status: 200, success: true, body: await response.json() };
+        case 401:
+            return { status: 401, success: false }; // Either the JWT is expired or invalid or the user has been deleted.
+        default:
+            return { status: 500, success: false };
         }
     } catch {
-        return { status: 500, success: false }
+        return { status: 500, success: false };
     }
 }
