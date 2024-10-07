55c22a27e2ceb686e71161c9f0af496bc88b5512
Clement-Z4RM <clement.lienard@epitech.eu>
2024-10-07 04:33:15 +0200

    feat(frontend-dashboard): create store service actions and reactions in arrays and display auth type [WIP]

--- frontend/src/routes/[lang=lang]/dashboard/+page.svelte
+++ frontend/src/routes/[lang=lang]/dashboard/+page.svelte
@@ -1,26 +1,36 @@
 <script lang="ts">
-    import type { PageData } from "./$types";
+    import type { PageServerData } from "./$types";
+    import type { Services } from "~/app";
     import { buttonVariants } from "$lib/components/ui/button";
     import * as Dialog from "$lib/components/ui/dialog";
     import Combobox from "$lib/components/dashboard/area/Combobox/Combobox.svelte";
     import type { Choice } from "$lib/components/dashboard/area/Combobox/Combobox";
     import LL from "$i18n/i18n-svelte";
 
-    export let data: PageData;
+    export let data: PageServerData;
 
     const services = data.services;
-    const actions = services?.filter(service => service.actions && service.actions.length) || [];
-    const reactions = services?.filter(service => service.reactions && service.reactions.length) || [];
+    const servicesActions = services?.filter(service => service.actions && service.actions.length) || [];
+    const servicesReactions = services?.filter(service => service.reactions && service.reactions.length) || [];
 
-    const actionsList = actions.reduce((acc: Choice[], { name, actions }) => acc.concat(actions.map(action => ({
-        label: `${name} - ${action.name}`, // TODO: i18n & good looking & PUTAING DE TYPES
-        value: `${name}.${action.name}`
-    }))), []);
+    const actions: Record<string, Services[number]["actions"][number]> = {};
+    const reactions: Record<string, Services[number]["reactions"][number]> = {};
 
-    const reactionsList = reactions.reduce((acc: Choice[], { name, reactions }) => acc.concat(reactions.map(reaction => ({
-        label: `${name} - ${reaction.name}`, // TODO: i18n & good looking & PUTAING DE TYPES
-        value: `${name}.${reaction.name}`
-    }))), []);
+    const actionsList = servicesActions.reduce((acc: Choice[], serviceAction) => acc.concat(serviceAction.actions.map(action => {
+        const id = `${serviceAction.name}.${action.name}`;
+
+        actions[id] = action;
+        // TODO: i18n & good looking & PUTAING DE TYPES
+        return { label: `${serviceAction.name} - ${action.name}`, value: id };
+    })), []);
+
+    const reactionsList = servicesReactions.reduce((acc: Choice[], serviceReaction) => acc.concat(serviceReaction.reactions.map(reaction => {
+        const id = `${serviceReaction.name}.${reaction.name}`;
+
+        reactions[id] = reaction;
+        // TODO: i18n & good looking & PUTAING DE TYPES
+        return { label: `${serviceReaction.name} - ${reaction.name}`, value: id };
+    })), []);
 
     let action: string = "";
     let reaction: string = "";
@@ -41,6 +51,15 @@
             <div class="grid gap-4 py-4">
                 <Combobox title="Action" choices={actionsList} value={action} setValue={(value) => action = value} />
                 <Combobox title="REAction" choices={reactionsList} value={reaction} setValue={(value) => reaction = value} />
+                {#if action}
+                    {#if actions[action].auth === "oauth"}
+                        <p>OAuth</p>
+                    {:else if actions[action].auth === "apiKey"}
+                        <p>API Key</p>
+                    {:else if actions[action].auth === "webhook"}
+                        <p>Webhook</p>
+                    {/if}
+                {/if}
             </div>
         </Dialog.Content>
     </Dialog.Root>
