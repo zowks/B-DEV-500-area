d02cc6f69128bbe06e974998e0c986a6425c2ad1
Clement-Z4RM <clement.lienard@epitech.eu>
2024-10-07 04:50:07 +0200

    fix(frontend-dashboard): get access token from `locals.client` and replace status code from `200` to `201` in `api.area.getAll`

--- frontend/src/routes/[lang=lang]/dashboard/+page.server.ts
+++ frontend/src/routes/[lang=lang]/dashboard/+page.server.ts
@@ -3,14 +3,13 @@ import api from "@common/api/api";
 import { env } from "$env/dynamic/private";
 import { error, redirect } from "@sveltejs/kit";
 
-export const load: PageServerLoad = async ({ parent, cookies }) => {
-    const { locale, services } = await parent();
+export const load: PageServerLoad = async ({ parent }) => {
+    const { locale, services, client } = await parent();
 
-    const accessToken = cookies.get("accessToken");
-    if (!accessToken)
+    if (!client)
         return error(401, "Unauthorized");
 
-    const areas = await api.area.getAll(env.API_URL, accessToken);
+    const areas = await api.area.getAll(env.API_URL, client.accessToken);
     if (areas.status === 401)
         return redirect(302, `/${locale}/auth/sign-in`);
     if (!areas.success)
