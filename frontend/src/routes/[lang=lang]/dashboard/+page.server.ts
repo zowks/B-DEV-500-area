41feb610750c9154fcd6d3bbae91623bfe030b2a
Zowks-VI <cyffers.merlin@gmail.com>
2024-10-07 02:29:45 +0000

    feat(frontend-dashboard): Adding `areas` to `PageData`

--- frontend/src/routes/[lang=lang]/dashboard/+page.server.ts
+++ frontend/src/routes/[lang=lang]/dashboard/+page.server.ts
@@ -1,7 +1,20 @@
 import type { PageServerLoad } from "./$types";
+import api from "@common/api/api";
+import { env } from "$env/dynamic/private";
+import { error, redirect } from "@sveltejs/kit";
 
-export const load: PageServerLoad = async ({ parent }) => {
+export const load: PageServerLoad = async ({ parent, cookies }) => {
     const { locale, services } = await parent();
 
-    return { locale, services };
+    const accessToken = cookies.get("accessToken");
+    if (!accessToken)
+        return error(401, "Unauthorized");
+
+    const areas = await api.area.getAll(env.API_URL, accessToken);
+    if (areas.status === 401)
+        return redirect(302, `/${locale}/auth/sign-in`);
+    if (!areas.success)
+        return error(500, "Internal Server Error");
+
+    return { locale, services, areas: areas.body };
 };
