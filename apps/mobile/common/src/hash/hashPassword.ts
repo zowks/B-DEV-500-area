5d7990680dc3e0c11f8f550896cc5c03440ac0f6
Clement-Z4RM <clement.lienard@epitech.eu>
2024-10-12 15:03:26 +0200

    refactor(mobile): remove `common` folder because real one will eb able to be used in favor of monorepo

--- apps/mobile/common/src/hash/hashPassword.ts
+++
@@ -1,5 +0,0 @@
-import hashString from "./hashString";
-
-export default async function hasPassword(password: FormDataEntryValue | null) {
-    return await hashString(password?.toString() || "");
-}
--- apps/mobile/common/src/types/auth/dto/register.dto.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class RegisterDto {
-    readonly email: string;
-    readonly password: string;
-    readonly firstname: string;
-    readonly lastname: string;
-    readonly has_accepted_terms_and_conditions: boolean;
-}
