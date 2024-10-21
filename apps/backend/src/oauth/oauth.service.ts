7c00a7393d9544ca5a136f73a15987f0fea3253b
ElouanR <elouan.rigomont@epitech.eu>
2024-10-21 18:14:02 +0200

    fix(backend/dev): add missing twitch OAuth service import

--- apps/backend/src/oauth/oauth.service.ts
+++ apps/backend/src/oauth/oauth.service.ts
@@ -2,18 +2,21 @@ import { Injectable } from "@nestjs/common";
 import { OAuthManager } from "./oauth.interface";
 import { GoogleOAuthService } from "./google/google.service";
 import { DiscordOAuthService } from "./discord/discord.service";
+import { TwitchOAuthService } from "./twitch/twitch.service";
 
 @Injectable()
 export class OAuthService {
     constructor(
         private readonly googleOAuthService: GoogleOAuthService,
-        private readonly discordOAuthService: DiscordOAuthService
+        private readonly discordOAuthService: DiscordOAuthService,
+        private readonly twitchOAuthService: TwitchOAuthService
     ) {}
 
     getOAuthCredentialsManager(name: string): OAuthManager {
         return {
             youtube: this.googleOAuthService,
-            discord: this.discordOAuthService
+            discord: this.discordOAuthService,
+            twitch: this.twitchOAuthService
         }[name];
     }
 }
