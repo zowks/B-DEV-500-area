f7b4690472cd5ba23f81a78e6ae3bfec23fd233b
DURAND Malo <malo.durand@epitech.eu>
2024-10-22 18:41:29 +0200

    fix(backend): lint

--- apps/backend/src/area/services/discord/discord.transformers.spec.ts
+++ apps/backend/src/area/services/discord/discord.transformers.spec.ts
@@ -2,11 +2,11 @@ import { transformAreaDiscordEmbedToRaw } from "./discord.transformers";
 import { AreaDiscordEmbed } from "./interfaces/discordEmbed.interface";
 
 describe("AREA Discord embed to raw Discord embed", () => {
-    it("should transform and AREA Discord embed to an object the Discord API can handle", ()=>{
+    it("should transform and AREA Discord embed to an object the Discord API can handle", () => {
         const areaDiscordEmbed: AreaDiscordEmbed = {
             authorName: "AUTHOR_NAME",
             authorUrl: "AUTHOR_URL",
-            color: 0xFFFFFF,
+            color: 0xffffff,
             description: "DESCRIPTION",
             footer: "FOOTER",
             imageUrl: "IMAGE_URL",
@@ -21,15 +21,15 @@ describe("AREA Discord embed to raw Discord embed", () => {
             title: areaDiscordEmbed.title,
             author: {
                 name: areaDiscordEmbed.authorName,
-                url: areaDiscordEmbed.authorUrl,
+                url: areaDiscordEmbed.authorUrl
             },
             color: areaDiscordEmbed.color,
             description: areaDiscordEmbed.description,
             footer: {
                 text: areaDiscordEmbed.footer
             },
-            image: {url: areaDiscordEmbed.imageUrl},
-            thumbnail: {url: areaDiscordEmbed.thumbnailUrl},
+            image: { url: areaDiscordEmbed.imageUrl },
+            thumbnail: { url: areaDiscordEmbed.thumbnailUrl },
             timestamp: areaDiscordEmbed.timestamp,
             type: areaDiscordEmbed.type,
             url: areaDiscordEmbed.url
